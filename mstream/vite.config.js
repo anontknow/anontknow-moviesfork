ab9b2d0e87958b09cfced68b78e9880944947238
Carlo Dee <carlodee.official@proton.me>
2025-11-11 12:46:40 +0800

    Removed PWA.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- mstream/vite.config.js
+++ mstream/vite.config.js
@@ -1,39 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    plugins: [
-      react(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest: {
-          name: 'MStream',
-          short_name: 'MStream',
-          description: 'Your favorite destination for movies and TV shows',
-          theme_color: '#e50914',
-          background_color: '#141414',
-          display: 'standalone',
-          icons: [
-            {
-              src: '/icons/icon-192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: '/icons/icon-512.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
-        }
-      })
-    ],
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./"),
